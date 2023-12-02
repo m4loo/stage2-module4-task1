c382c8150c19be3c948836662ae7bf018fcf97db
m4loo <128167781+m4loo@users.noreply.github.com>
2023-12-02 13:07:45 +0600

    done?

--- task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
+++ task3-factory-pattern/src/main/java/com/mjc/stage2/impl/H2ConnectionFactory.java
@@ -1,6 +1,45 @@
 package com.mjc.stage2.impl;
 
-public class H2ConnectionFactory {
-    // Write your code here!
-}
+import com.mjc.stage2.ConnectionFactory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class H2ConnectionFactory implements ConnectionFactory {
+
+    @Override
+    public Connection createConnection() {
+        Connection connection = null;
+
+        Properties props = new Properties();
+        try {
+            props.load(getClass().getResourceAsStream("/h2database.properties"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Get the JDBC driver class
+        String driver = props.getProperty("jdbc_driver");
+        if (driver != null) {
+            try {
+                Class.forName(driver);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String url = props.getProperty("db_url");
+        String username = props.getProperty("user");
+        String password = props.getProperty("password");
+
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
